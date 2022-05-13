c3080b819382925c2fff2dadbfd9687bea01f992
celaskane <leandrou9@gmail.com>
2022-05-13 19:08:43 -0300

    Fim da Listagem 2.3.4
    Componente principal devolve a JSX que representa a estrutura de
    navegação entre telas.

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import container from './navegacao/LugaresNavigator'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return container
 }
 
 const styles = StyleSheet.create({
